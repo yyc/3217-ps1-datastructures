7b6429fb6ba0aaea9562c9ba2fd76f6528e28dd0
Yuchuan <yyc478@gmail.com>
2017-01-12 17:12:46 +0800

    Add passing implementation of Queue

--- DataStructures/Queue.swift
+++ DataStructures/Queue.swift
@@ -12,52 +12,66 @@ enum QueueError: Error {
 /**
  A generic `Queue` class whose elements are first-in, first-out.
 
- - Authors: CS3217
+ - Authors: CS3217, Yuan Yuchuan
  - Date: 2017
  */
 struct Queue<T> {
+    
+    private var queueArray = Array<T>()
+    private var head:Int = 0
+    private var tail:Int = 0
     /// Adds an element to the tail of the queue.
     /// - Parameter item: The element to be added to the queue
     mutating func enqueue(_ item: T) {
+        queueArray.append(item)
+        tail = tail + 1
     }
 
     /// Removes an element from the head of the queue and return it.
     /// - Returns: item at the head of the queue
     /// - Throws: QueueError.EmptyQueue
     mutating func dequeue() throws -> T {
-        // TODO: Replace/remove the following line in your implementation.
-        throw QueueError.emptyQueue
+        if(head == tail){
+            throw QueueError.emptyQueue
+        }
+        let value = queueArray[head]
+        head = head + 1
+        return value
     }
 
     /// Returns, but does not remove, the element at the head of the queue.
     /// - Returns: item at the head of the queue
     /// - Throws: QueueError.EmptyQueue
     func peek() throws -> T {
-        // TODO: Replace/remove the following line in your implementation.
-        throw QueueError.emptyQueue
+        if(head == tail){
+            throw QueueError.emptyQueue
+        }
+        return queueArray[head]
     }
 
     /// The number of elements currently in the queue.
     var count: Int {
-        // TODO: Replace/remove the following line in your implementation.
-        return 0
+        return tail - head
     }
 
     /// Whether the queue is empty.
     var isEmpty: Bool {
-        // TODO: Replace/remove the following line in your implementation.
-        return false
+        return tail == head
     }
 
     /// Removes all elements in the queue.
     mutating func removeAll() {
+        head = 0
+        tail = 0
     }
 
     /// Returns an array of the elements in their respective dequeue order, i.e.
     /// first element in the array is the first element to be dequeued.
     /// - Returns: array of elements in their respective dequeue order
     func toArray() -> [T] {
-        // TODO: Replace/remove the following line in your implementation.
-        return [T]()
+        if(self.isEmpty){
+            return Array<T>()
+        }
+        return Array(queueArray[Range(head..<tail)])
     }
 }
