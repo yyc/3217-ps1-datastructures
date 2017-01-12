84f980106f542a9b14b2a0c54a30945b5e975b0e
Yuchuan <yyc478@gmail.com>
2017-01-12 14:23:17 +0800

    Add passing implementation of Stack

--- DataStructures/Stack.swift
+++ DataStructures/Stack.swift
@@ -15,48 +15,52 @@ enum StackError: Error {
  - Date: 2017
  */
 struct Stack<T> {
+    var stackArray = Array<T>();
     /// Adds an element to the top of the stack.
     /// - Parameter item: The element to be added to the stack
     mutating func push(_ item: T) {
+        stackArray.append(item)
     }
 
     /// Removes the element at the top of the stack and return it.
     /// - Returns: element at the top of the stack
     /// - Throws: StackError.EmptyStack
     mutating func pop() throws -> T {
-        // TODO: Replace/remove the following line in your implementation.
-        throw StackError.emptyStack
+        if self.isEmpty{
+            throw StackError.emptyStack
+        }
+        return stackArray.popLast()!
     }
 
     /// Returns, but does not remove, the element at the top of the stack.
     /// - Returns: element at the top of the stack
     /// - Throws: StackError.EmptyStack
     func peek() throws -> T {
-        // TODO: Replace/remove the following line in your implementation.
-        throw StackError.emptyStack
+        if self.isEmpty{
+            throw StackError.emptyStack
+        }
+        return stackArray.last!
     }
 
     /// The number of elements currently in the stack.
     var count: Int {
-        // TODO: Replace/remove the following line in your implementation.
-        return 0
+        return stackArray.count
     }
 
     /// Whether the stack is empty.
     var isEmpty: Bool {
-        // TODO: Replace/remove the following line in your implementation.
-        return false
+        return count == 0
     }
 
     /// Removes all elements in the stack.
     mutating func removeAll() {
+        stackArray.removeAll()
     }
 
     /// Returns an array of the elements in their respective pop order, i.e.
     /// first element in the array is the first element to be popped.
     /// - Returns: array of elements in their respective pop order
     func toArray() -> [T] {
-        // TODO: Replace/remove the following line in your implementation.
-        return [T]()
+        return stackArray.reversed()
     }
 }
