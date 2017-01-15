2b23d4fd3ad18cbc21a74693cfbf107f78410458
Yuchuan <yyc478@gmail.com>
2017-01-15 11:38:39 +0800

    Add passing DepthFirstOrderGenerator

--- DataStructures/DepthFirstOrderGenerator.swift
+++ DataStructures/DepthFirstOrderGenerator.swift
@@ -9,12 +9,22 @@
  */
 struct DepthFirstOrderGenerator<Key: Hashable, Value: Collection> : IteratorProtocol, Sequence where Value.Iterator.Element == Key {
 
+    private var stack = Stack<Key>()
+    
+    // Use a set to ensure that no node is traversed twice
+    private var set = Set<Key>()
+    
+    private var graph : Dictionary<Key, Value>
+    
+    
     /// Constructs a `DepthFirstOrderGenerator` with the given graph and start
     /// node.
     /// - Parameters:
     ///   - graph: A dictionary of node to adjacency list pairs.
     ///   - start: The start node.
     init(graph: Dictionary<Key, Value>, start: Key) {
+        stack.push(start)
+        self.graph = graph
     }
 
     func makeIterator() -> DepthFirstOrderGenerator<Key, Value> {
@@ -23,6 +33,20 @@ struct DepthFirstOrderGenerator<Key: Hashable, Value: Collection> : IteratorProt
 
     mutating func next() -> Key? {
         // TODO: Replace/remove the following line in your implementation.
-        return nil
+        if(stack.isEmpty){
+            return nil
+        }
+        let key = try! stack.pop()
+        if set.contains(key){
+            return next()
+        }
+        set.insert(key)
+        for neighbour in graph[key]!.reversed(){
+            if(!set.contains(neighbour)){
+                // Add node to stack if it was not previously traversed
+                stack.push(neighbour)
+            }
+        }
+        return key
     }
 }
