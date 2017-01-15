d05c6d896786cc9294749b4d1de7e936abae6edd
Yuchuan <yyc478@gmail.com>
2017-01-15 11:39:59 +0800

    Add passing BreadthFirstOrderGenerator

--- DataStructures/BreadthFirstOrderGenerator.swift
+++ DataStructures/BreadthFirstOrderGenerator.swift
@@ -9,12 +9,18 @@
  */
 struct BreadthFirstOrderGenerator<Key: Hashable, Value: Collection> : IteratorProtocol, Sequence where Value.Iterator.Element == Key {
 
+    private var queue = Queue<Key>()
+    private var set = Set<Key>()
+    private var graph : Dictionary<Key, Value>
     /// Constructs a `BreadthFirstOrderGenerator` with the given graph and start
     /// node.
     /// - Parameters:
     ///   - graph: A dictionary of node to adjacency list pairs.
     ///   - start: The start node.
     init(graph: Dictionary<Key, Value>, start: Key) {
+        self.graph = graph
+        queue.enqueue(start)
+        set.insert(start)
     }
 
     func makeIterator() -> BreadthFirstOrderGenerator<Key, Value> {
@@ -22,7 +28,16 @@ struct BreadthFirstOrderGenerator<Key: Hashable, Value: Collection> : IteratorPr
     }
 
     mutating func next() -> Key? {
-        // TODO: Replace/remove the following line in your implementation.
-        return nil
+        if(queue.isEmpty){
+            return nil
+        }
+        let key = try! queue.dequeue()
+        for neighbour in graph[key]!{
+            if !set.contains(neighbour){
+                queue.enqueue(neighbour)
+                set.insert(neighbour)
+            }
+        }
+        return key
     }
 }
